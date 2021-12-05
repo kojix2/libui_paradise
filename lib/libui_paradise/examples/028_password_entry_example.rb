1834f36cd7cb2a6d388e63c99a57154f24d7e8d9
kojix2 <kojix2@users.noreply.github.com>
2021-12-05 10:02:31 +0000

    Update Gem copy

--- lib/libui_paradise/examples/028_password_entry_example.rb
+++ lib/libui_paradise/examples/028_password_entry_example.rb
@@ -9,9 +9,17 @@ require 'libui_paradise/autoinclude'
 window = margined_window('Password Entry Example', 280, 80, 0)
 
 outer_vbox = padded_vbox
-entry = ui_password_entry
+entry_containing_the_password = ui_password_entry
 outer_vbox.minimal(ui_text('Password: '), 0)
-outer_vbox.minimal(entry, 0)
+outer_vbox.minimal(entry_containing_the_password, 0)
+
+button = ui_button('Show the password')
+button.on_clicked {
+  _ = entry_containing_the_password.text?.to_s
+  e _
+  LibuiParadise.msg_box(window, _)
+}
+outer_vbox.minimal(button, 0)
 
 window.child = outer_vbox
 window.intelligent_quit
