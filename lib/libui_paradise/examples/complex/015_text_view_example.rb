b876240f2045bfde8f1c5521676c71305a183315
kojix2 <kojix2@users.noreply.github.com>
2022-07-27 10:02:40 +0000

    Update Gem copy

--- lib/libui_paradise/examples/complex/015_text_view_example.rb
+++ lib/libui_paradise/examples/complex/015_text_view_example.rb
@@ -9,8 +9,8 @@ require 'libui_paradise/autoinclude'
 window = padded_main_window('Text View Example', 500, 300, 0)
 
 outer_vbox = padded_vbox
-text = multiline_entry('Hello world! This is set to read only.')
-text.is_read_only
+text = multiline_entry('Hello world! This can be set to read only via .is_read_only().')
+# text.is_read_only
 outer_vbox.minimal(text, 1)
 window.child = outer_vbox
 window.intelligent_quit
